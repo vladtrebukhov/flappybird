cbec66de23b36dac174592d5bbcf7e3e6361eb8f
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-06-23 00:53:35 -0400

    Added score and collision mechanics

--- script.js
+++ script.js
@@ -23,13 +23,8 @@ var scoreSound = new Audio('sounds/score.mp3');
 
 // define variables
 const pipeWidth = 52;
-const pipenorthHeight = 242;
-const pipesouthHeight = 378;
 const foregroundHeight = 118;
-const birdWidth = 38;
-const birdHeight = 26;
 var score = 0;
-
 var pipe = [];
 
 pipe[0] = {
@@ -39,8 +34,8 @@ pipe[0] = {
 };
 
 var gap = 85;
-var constant = pipenorthHeight + gap; // pipe South starts after pipe north plus gap
-var gravity = 1.5;
+var constant = pipeNorth.height + gap; // pipe South starts after pipe north plus gap
+var gravity = 1.25;
 var birdX = 20;
 var birdY = 150;
 
@@ -48,13 +43,13 @@ var birdY = 150;
 
 var flyUp = () => {
   birdY = birdY - 25;
+  flySound.currentTime = 0;
   flySound.play();
 }
 document.addEventListener('keydown', flyUp);
 
 
 // draw function
-
 var draw = () => {
   context.save();
   context.drawImage(background, 0, 0); // drawImage(imageName, x, y)
@@ -65,33 +60,38 @@ var draw = () => {
     context.drawImage(foreground, 0, canvas.height - foreground.height);
     pipe[i].x--;
 
-    if (pipe[i].x === canvas.width - 188) {
+    if (pipe[i].x === canvas.width - 188) { // add new set of pipes to array pipe at X and Y once pipes get to about middle of canvas
       pipe.push({
         x: canvas.width,
-        y: Math.floor(Math.random() * pipenorthHeight) - pipenorthHeight
+        y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
 
       })
     }
 
-    context.drawImage(bird, birdX, birdY);
-    birdY = birdY + gravity; // y increases by value of gravity each frame to make bird drop
-
-    if (birdY + birdHeight >= canvas.height - foreground.height || birdX + birdWidth >= pipe[i].x) {
-      location.reload();
+    if (pipe[i].x === birdX + bird.width - 30) { // increase score once bird and pipes intersect
+      score++
+      scoreSound.play();
     }
 
-    if (birdX + birdWidth > pipe[i].x) {
-      score += 1
+    // collision detection
+    if (birdX + bird.width >= pipe[i].x && birdX <= pipe[i].x + pipeNorth.width &&
+    (birdY <= pipe[i].y + pipeNorth.height || birdY + bird.height >= pipe[i].y + constant) ||
+  birdY + bird.height >= canvas.height - foreground.height) {
+      location.reload()
     }
-
-    context.fillStyle = '#000';
-    context.font = '20px Verdana';
-    context.fillText(`Score: ${score}`, 100, 450);
   }
+  // load score text and style
+  context.fillStyle = '#000';
+  context.font = '25px Arial';
+  context.fillText(`Score: ${score}`, 100, 450);
+
+
+
+  context.drawImage(bird, birdX, birdY);
+  birdY += gravity; // y increases by value of gravity each frame to make bird drop
 
   window.requestAnimationFrame(draw);
   context.restore();
 }
 
 window.onload = draw;
-
